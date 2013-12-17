14475777eeede8ac0e420964472013d472a1145c
Bill Reed <billreed72@gmail.com>
2013-12-17 09:22:52 -0600

    Update createTestFiles.sh

--- createTestFiles.sh
+++ createTestFiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-read -p "How many test files would you like to create? " N
-read -p "How large would you like your test files? (i.e. 10m = 10 mb): " size
-read -p "Enter a prefix name for your test files. (i.e. testFile): " prefix
-x=0
+read -p "How many files do you want to create? :" N
+read -p "What size do you want each file? (size[b|k|m|g] i.e. 10m=10MB) :" size
+read -p "Enter a prefix name for your test files. (i.e. testFile) :" prefix
+x=1
 y=$N
 size=$size
 prefix=$prefix
-for ((i=x; i<=y; i++)); do mkfile $size $prefix$i.dat; 
+for ((i=x; i<=y; i++)); do mkfile $size $prefix$i.txt; 
 ls -lh $prefix*;done

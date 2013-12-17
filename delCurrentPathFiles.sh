23de95c75f4529827858d5c5748ec31bf44c93b7
Bill Reed <billreed72@gmail.com>
2013-12-17 09:32:50 -0600

    Update delCurrentPathFiles.sh

--- delCurrentPathFiles.sh
+++ delCurrentPathFiles.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 while true; do
-    read -p "Would you like to delete some files in this directory? " yn
+    read -p "Ready to Delete some files? (y/n): " yn
     case $yn in
-        [Yy]* ) read -p "Enter a search string to delete files: (i.e. *part-of-name*) " userInput
-				echo "Files containing "$userInput" are being deleted now. ";
-	            rm -fv $userInput;
-				ls -lh; break;;
+        [Yy]* ) read -p "Enter a search string to delete files (i.e. *part-of-name*): " userInput
+	            rm -fv $userInput;ls -lh;
+				echo "File names containing " $userInput " have been deleted. "; break;;
         [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
+        * ) echo "Please answer Y or N.";;
     esac
 done

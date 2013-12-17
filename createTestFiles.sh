96348f229448e9e9a18d684417b95fcbf8f0bb04
Bill Reed <billreed72@gmail.com>
2013-12-16 22:24:21 -0600

    Update createTestFiles.sh
    Added more variables

--- createTestFiles.sh
+++ createTestFiles.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 read -p "How many test files would you like to create? " N
-read -p "How large would you like your test files? (i.e. 10m = 10 mb) " Z
+read -p "How large would you like your test files? (i.e. 10m = 10 mb): " size
+read -p "Enter a prefix name for your test files. (i.e. testFile): " prefix
 x=0
 y=$N
-z=$Z
-for ((i=x; i<=y; i++)); do mkfile $Z output$i.dat; done
+size=$size
+prefix=$prefix
+for ((i=x; i<=y; i++)); do mkfile $size $prefix$i.dat; 
+ls -lh $prefix*;done

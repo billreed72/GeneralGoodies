a003c0d419bc3cc40386f6d44df48c9bc43caebe
Bill Reed <billreed72@gmail.com>
2014-01-20 23:44:48 -0500

    Update BOSS.sh

--- BOSS.sh
+++ BOSS.sh
@@ -1,65 +1,112 @@
 #!/bin/bash
+#=================================================================
+# MAIN MENU
+#=================================================================
 function mainMenu {
   mainAnswer=""
   while [ "$mainAnswer" != "x" ];do
-    tput clear;tput cup 3 15;tput setaf 1;tput setab 7
+    tput clear;tput cup 3 15;tput setaf 214;tput setab 256; tput rev; tput bold
     echo "Bill's Operations Shell System (BOSS)";tput sgr0
-    tput cup 5 17;tput rev;echo 'Main Menu Heading';tput sgr0
-    tput cup 7 17;echo '1. GAM Commands'
-    tput cup 8 17;echo '2. Fringe Commands'
-    tput cup 9 17;echo 'x. Quit the application'
-    tput cup 10 17;tput bold;read -p "MM - Enter your choice [1-2 or x]" mainAnswer
+    tput cup 5 17;tput bold; tput setaf 0;tput setab 10;echo 'Main Menu';tput sgr0
+    tput cup 7 17;echo '1. Fringe'
+    tput cup 8 17;echo '2. GAM'
+    tput cup 9 17;echo '3. PowerTools'
+    tput cup 10 17;echo 'x. Quit'
+    tput cup 11 17;tput bold;read -p "Main Menu: Enter your choice [1-2 or x]:" mainAnswer; tput sgr0
     tput clear
-      if [ "$mainAnswer" = "1" ]; then subMenu1
-      elif [ "$mainAnswer" = "2" ]; then subMenu2
-      elif [ "$mainAnswer" = "x" ];then break
+      if [ "$mainAnswer" = "1" ]; then fringeMenu
+      elif [ "$mainAnswer" = "2" ]; then gamMenu
+      elif [ "$mainAnswer" = "3" ]; then powerToolsMenu
+      elif [ "$mainAnswer" = "x" ]; then break
+      elif [ "$mainAnswer" = "X" ]; then break
       fi
     tput sgr0
   done }
-function subMenu1 {
-  sm1Answer=""
-  while [ "$sm1Answer" != "x" ];do
-    tput cup 3 15;tput setaf 1;tput setab 7
+#=================================================================
+# GAM MENU
+#=================================================================
+function gamMenu {
+  gamAnswer=""
+  while [ "$gamAnswer" != "x" ];do
+    tput clear;tput cup 3 15;tput setaf 214;tput setab 256
     echo "Bill's Operations Shell System (BOSS)";tput sgr0
     tput cup 5 17;tput rev;echo 'GAM Commands';tput sgr0
-    tput cup 7 17;echo '1. Get Google Apps User Report'
-    tput cup 8 17;echo '2. Calendar'
+    tput cup 7 17;echo '1. Google Apps User Report (single user)'
+    tput cup 8 17;echo '2. Google Apps User Report (ALL users)'
     tput cup 9 17;echo 'x. Main Menu'
-    tput bold;tput cup 10 17;read -p "SM1 - Enter your choice [1-2 or x]:" sm1Answer
-    tput clear
-      if [ "$sm1Answer" = "1" ]; then getUserReport
-      elif [ "$sm1Answer" = "2" ]; then tput cup 20; cal
-      elif [ "$sm1Answer" = "x" ]; then mainMenu
+    tput bold;tput cup 10 17;read -p "GAM Menu: Enter your choice [1-2 or x]:" gamAnswer
+      if [ "$gamAnswer" = "1" ]; then getUserReport
+      elif [ "$gamAnswer" = "2" ]; then getUserReportALL
+      elif [ "$gamAnswer" = "x" ]; then mainMenu
+      elif [ "$gamAnswer" = "X" ]; then mainMenu
       fi
     tput sgr0
   done }
-function subMenu2 {
-  sm2Answer=""
-  while [ "$sm2Answer" != "x" ];do
-    tput cup 3 15;tput setaf 1;tput setab 7
+#=================================================================
+# FRINGE MENU
+#=================================================================
+function fringeMenu {
+  fringeAnswer=""
+  while [ "$fringeAnswer" != "x" ];do
+    tput clear;tput cup 3 15;tput setaf 214;tput setab 256
     echo "Bill's Operations Shell System (BOSS)";tput sgr0
     tput cup 5 17;tput rev;echo 'Fringe Commands';tput sgr0
-    tput cup 7 17;echo '1. Get a List of Drive Files for a user'
-    tput cup 8 17;echo '2. SM2 Option B'
+    tput cup 7 17;echo '1. List of Drive Files for a user'
+    tput cup 8 17;echo '2. Display the calendar'
     tput cup 9 17;echo 'x. Main Menu'
-    tput bold;tput cup 10 17;read -p "SM2 - Enter your choice [1-2 or x]:" sm2Answer    
-    tput clear
-      if [ "$sm2Answer" = "1" ]; then getDriveFilesWithFringe
-      elif [ "$sm2Answer" = "2" ]; then tput cup 20; cal
-      elif [ "$sm2Answer" = "x" ]; then mainMenu
+    tput bold;tput cup 10 17;read -p "Fringe Menu: Enter your choice [1-2 or x]:" fringeAnswer    
+      if [ "$fringeAnswer" = "1" ]; then getDriveFilesWithFringe
+      elif [ "$fringeAnswer" = "2" ]; then tput setaf 256; tput cup 20; cal; sleep 1; tput sgr0
+      elif [ "$fringeAnswer" = "x" ]; then mainMenu
+      elif [ "$fringeAnswer" = "X" ]; then mainMenu
       fi
     tput sgr0
   done }
+#=================================================================
+# POWERTOOLS MENU
+#=================================================================
+function powerToolsMenu {
+  powerToolsAnswer=""
+  while [ "$powerToolsAnswer" != "x" ];do
+    tput clear;tput cup 3 15;tput setaf 214;tput setab 256
+    echo "Bill's Operations Shell System (BOSS)";tput sgr0
+    tput cup 5 17;tput rev;echo 'PowerTools Commands';tput sgr0
+    tput cup 7 17;echo '1. WHAT IS YOUR FAVORITE COLOR? enter a number...'
+    tput cup 10 17;echo 'x. Main Menu'
+    tput bold;tput cup 19 17;read -p "PT - Enter your choice [1-? or x]:" powerToolsAnswer
+      if [ "$powerToolsAnswer" > "0" ]; then tput setaf $powerToolsAnswer; tput cup 20; cal; sleep 1; tput sgr0
+      elif [ "$powerToolsAnswer" = "x" ]; then mainMenu
+      elif [ "$powerToolsAnswer" = "X" ]; then mainMenu
+      fi
+    tput sgr0
+  done }
+#=================================================================
+# GAM FUNCTIONS
+#=================================================================
 function getUserReport {
   tput bold;tput cup 12 17;read -p 'Enter a username or email address:' gAppsUser
   tput sgr0
   tput cup 14 17
-    # Syntax: gam report users [todrive] [date <yyyy-dd-mm>] [user <email>] [filter <filter terms>] [fields <included fields>]
     python /Users/billreed/Desktop/Development/BOSS/appirio-dev1GAM/gam.py \
     report users \
     user $gAppsUser > UserReport.csv
+  sleep 3
   tput clear
   }
+#=================================================================
+function getUserReportALL {
+  answer=""
+  while true; do
+    read -p "Do you want to continue (Y/N)?" answer
+    case $answer in
+        [Yy]* ) python /Users/billreed/Desktop/Development/BOSS/appirio-dev1GAM/gam.py report users > AllUsersReport.csv; tput clear; subMenu1;;
+        [Nn]* ) tput setaf 10;echo "You chose wisely. :-) "; sleep 1; tput sgr0; break;;
+        * ) echo "Please answer yes or no!!";;
+    esac
+done }
+#=================================================================
+# FRINGE FUNCTIONS
+#=================================================================
 function getDriveFilesWithFringe {
   tput bold;tput cup 12 17;read -p "Enter a user's email address:" gAppsFringeUser
   tput sgr0
@@ -78,4 +125,5 @@ python /Users/billreed/Desktop/Development/BOSS/fringe/fringe-dev.py \
   rm "Drive.Files.appirio-dev1.com"
   tput clear
   }
+#=================================================================
 mainMenu
